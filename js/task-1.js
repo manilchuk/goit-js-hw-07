3f0627d44f189c67a081233756b86f25834580b0
Valentyn <manilchuk.v@gmail.com>
2026-02-12 20:15:06 +0200

    add html, css, js files and performed 1-3 HW

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+const allItems = document.querySelectorAll(".item");
+console.log(`Number of categories: ${allItems.length}`);
+for (const item of allItems) {
+  const title = item.querySelector("h2");
+  console.log(`Category: ${title.textContent}`);
+  const elements = item.querySelectorAll("li");
+  console.log(`Elements: ${elements.length}`);
+}
