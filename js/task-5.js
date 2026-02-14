3cea520649cced799d3427d531f92b76448f2554
Valentyn <manilchuk.v@gmail.com>
2026-02-14 17:41:02 +0200

    added styles and performed HW 4, 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,13 @@
+const changeColor = document.querySelector(".change-color");
+const bodyElem = document.querySelector("body");
+const colorText = document.querySelector(".color");
+
+changeColor.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  bodyElem.style.backgroundColor = color;
+  colorText.textContent = color;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
